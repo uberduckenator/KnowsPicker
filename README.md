4b2bb0bb81648fd343339a5ce821dca2dd949159
Parimal Patel <parimal98@hotmail.com>
2019-11-19 19:01:34 -0500

    Login works, changed user_id to login_id

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# KnowsPicker - Pari
+# KnowsPicker
 Vanier E-Commerce final project
----Pari's Workspace---
--- app/views/Login/index.php
+++ app/views/Login/index.php
@@ -24,7 +24,7 @@
 	<div class="form-group">
 		<input type="submit" name="action" value="Login" />
 	</div>
-	<a href="../register">Don't have an account? Register here.</a> 
+	<a href="/Login/register">Don't have an account? Register here.</a> 
 	</form>
 </div>
 </body></html>
