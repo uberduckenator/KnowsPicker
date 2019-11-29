4228624bde930521e3e5a28cd714c7495851ba4c
Parimal Patel <parimal98@hotmail.com>
2019-11-29 11:48:07 -0500

    Fixed some issues

--- app/views/Company/inventory.php
+++ app/views/Company/inventory.php
@@ -1,19 +1,21 @@
 <?php
 include("header.php");
 ?>
+
 <h1>Inventory</h1>
 
-<a href="/Company/additem">Add item</a>
+<a href="/Company/addItems">Add item</a>
 
 <table>
 	<tr><th>Picture &nbsp;</th><th>item_name &nbsp;</th><th>price &nbsp;</th><th>item_type &nbsp;</th><th>stock &nbsp;</th><th>rebate &nbsp;</th><th>Controls</th></tr>
-<?php
-foreach($model as $items){
-	$item_id = $items->item_id;
-	echo "<img alt=picture for item>";
-	echo "<tr><th>$items->item_name</th><th>$items->price</th><th>$items->item_type</th><th>$items->stock</th><th>$items->rebate</th>";
-	echo "<th><a href=Items/edit/$item_id>Edit</a><a href=Items/details/$item_id>Details</a><a href=Items/Delete/$item_id>Delete</a></th></tr>";
-?>
+	<?php
+	foreach($model as $items){
+		$item_id = $items->item_id;
+		echo "<img alt=picture for item>";
+		echo "<tr><th>$items->item_name</th><th>$items->price</th><th>$items->item_type</th><th>$items->stock</th><th>$items->rebate</th>";
+		echo "<th><a href=/Items/edit/$item_id>Edit</a><a href=/Items/details/$item_id>Details</a><a href=/Items/Delete/$item_id>Delete</a></th></tr>";
+	}
+	?>
 </table>
 
 <?php
